7b82e3de9f01fce504eeef695e3d91f0fbc12c3e
Tambouil <v.berceaux@gmail.com>
2022-08-19 13:56:04 +0200

    chore: public static folder

--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,23 @@
 import Fastify, { FastifyReply, FastifyRequest } from "fastify";
-import jwt, { JWT } from "@fastify/jwt";
-import fastifyEnv from "@fastify/env";
-
-declare module "fastify" {
-  interface FastifyRequest {
-    jwt: JWT;
-  }
-  export interface FastifyInstance {
-    config: {
-      JWT_SECRET_KEY: string;
-    };
-    authenticate: (
-      request: FastifyRequest,
-      reply: FastifyReply
-    ) => Promise<void>;
-  }
-}
+import jwt from "@fastify/jwt";
+import { config } from "./utils/config";
+import fastifyStatic from "@fastify/static";
+import path from "path";
 
 const fastify = Fastify({
   logger: true,
 });
 
+/** static folder plugin **/
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, "public"),
+  prefix: "/public/",
+});
+
 /** jwt plugin **/
-// const envSchema = {
-//   type: "object",
-//   required: ["JWT_SECRET_KEY"],
-//   properties: {
-//     JWT_SECRET_KEY: {
-//       type: "string",
-//       default: "mysecretpassword",
-//     },
-//   },
-// };
-
-// const envOptions = {
-//   schema: envSchema,
-//   dotenv: true,
-//   confKey: "config",
-// };
-
-// const initialize = async () => {
-//   await fastify.register(fastifyEnv, envOptions);
-
-//   await fastify.register(jwt, {
-//     secret: fastify.config.JWT_SECRET_KEY,
-//   });
-// };
-// initialize();
+fastify.register(jwt, {
+  secret: config.JWT_SECRET_KEY,
+});
 
 fastify.decorate(
   "authenticate",
